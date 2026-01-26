3f20948bbecde93e6d5293932e511d717a1b861f
fynnob <112804669+fynnob@users.noreply.github.com>
2026-01-26 19:27:54 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdgoyDsUteoRBIQ74NXvg5Xfj8V105Dnw",
+  authDomain: "chirpssgames.firebaseapp.com",
+  databaseURL: "https://chirpssgames-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chirpssgames",
+  storageBucket: "chirpssgames.firebasestorage.app",
+  messagingSenderId: "949878456642",
+  appId: "1:949878456642:web:4287fe5fe9577ce1b688db",
+  measurementId: "G-E628QMNRQY"
+};
+
+firebase.initializeApp(firebaseConfig);
